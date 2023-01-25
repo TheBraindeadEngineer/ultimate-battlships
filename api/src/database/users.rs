03f1cd8272c94eeffe7250fe7d84df4c13ff81a8
Elvin Bajric <190026@studierende.htl-donaustadt.at>
2023-01-26 00:47:31 +0100

    added some more endpoints and logic for api

--- api/src/database/users.rs
+++ api/src/database/users.rs
@@ -16,7 +16,21 @@ pub struct User {
     pub password: String,
 }
 
+pub fn check_if_exists_user(conn: &mut PgConnection, check_username: &str) -> bool {
+    use crate::schema::users::dsl::*;
+    let results = users.filter(username.eq(check_username)).load::<User>(conn);
+
+    match results {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
 pub fn insert_user(conn: &mut PgConnection, username: &str, password: &str) -> Option<User> {
+    if check_if_exists_user(conn, username) {
+        return None;
+    }
+
     let new_user = NewUser { username, password };
 
     let insert = diesel::insert_into(users::table)
@@ -26,5 +40,29 @@ pub fn insert_user(conn: &mut PgConnection, username: &str, password: &str) -> O
     match insert {
         Ok(value) => Some(value),
         Err(_) => None,
+    }
+}
+
+pub fn check_user(conn: &mut PgConnection, check_username: &str, check_password: &str) -> bool {
+    use crate::schema::users::dsl::*;
+    let results = users
+        .filter(username.eq(check_username))
+        .filter(password.eq(check_password))
+        .load::<User>(conn);
+
+    match results {
+        Ok(_) => true,
+        _ => false,
+    }
+}
+
+pub fn delete(conn: &mut PgConnection, user_id: i32) -> bool {
+    use crate::schema::users::dsl::*;
+
+    let result = diesel::delete(users.filter(id.eq(user_id))).execute(conn);
+
+    match result {
+        Ok(_) => true,
+        _ => false,
     }
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -6,14 +6,5 @@ use database::*;
 fn main() {
     println!("under construction");
     let connection = &mut establish_connection();
-    let game1 = games::new(connection, 1, 2);
-    println!("{:?}", game1);
-    let game2 = games::new(connection, 1, 2);
-    println!("{:?}", game2);
-    println!("{:?}", games::get_all(connection).unwrap());
-    let game = games::delete(connection, 1);
-    println!("{:?}", game);
-    let game = games::delete(connection, 2);
-    println!("{:?}", game);
     println!("{:?}", games::get_all(connection).unwrap());
 }
